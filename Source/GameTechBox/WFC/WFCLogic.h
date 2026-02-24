045c8ed943272902427390d33d711208a2b61079
lbeeno09 <33786611+lbeeno09@users.noreply.github.com>
2026-02-24 14:04:51 +0900

    WFC

---
+++ Source/GameTechBox/WFC/WFCLogic.h
@@ -0,0 +1,18 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+
+struct FTileRule
+{
+	int32 TileID;
+	int32 EdgesIDs[4]; // 0: +X, 1: +Y, 2: -X, 3: -Y;
+};
+
+struct FWFCNode
+{
+	uint64 Possibilities; // Bitmask; 1: Possible, 0: Pruned
+	bool bCollapsed = false;
+	int32 FinalTileIndex = -1;
+
+	int32 GetEntropy() const { return FMath::CountBits(Possibilities); }
+};
