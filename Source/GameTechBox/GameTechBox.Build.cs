d870ae6255e1d0feec34ec1bd7ffbf34248bd156
lbeeno09 <33786611+lbeeno09@users.noreply.github.com>
2026-03-03 13:20:57 +0900

    transfer

--- Source/GameTechBox/GameTechBox.Build.cs
+++ Source/GameTechBox/GameTechBox.Build.cs
@@ -1,5 +1,6 @@
 // Copyright Epic Games, Inc. All Rights Reserved.
 
+using System.IO;
 using UnrealBuildTool;
 
 public class GameTechBox : ModuleRules
@@ -11,6 +12,8 @@ public class GameTechBox : ModuleRules
 		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput" });
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
+
+		PublicIncludePaths.Add(Path.Combine(ModuleDirectory, "Public"));
 
 		// Uncomment if you are using Slate UI
 		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
